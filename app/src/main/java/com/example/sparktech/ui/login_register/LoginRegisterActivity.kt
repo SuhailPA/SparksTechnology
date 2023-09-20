564aa3c882d556d58de0641973f510d591f42f9b
Suhail P A <Suhailabbas23@gmail.com>
2023-09-20 17:21:53 +0530

    Login Screen Ui Completed

--- app/src/main/java/com/example/sparktech/ui/login_register/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/sparktech/ui/login_register/LoginRegisterActivity.kt
@@ -1,9 +1,10 @@
-package com.example.sparktech
+package com.example.sparktech.ui.login_register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sparktech.R
 
-class MainActivity : AppCompatActivity() {
+class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/sparktech/ui/login_register/LoginRegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sparktech.ui.login_register
+
+import androidx.lifecycle.ViewModel
+
+class LoginRegisterViewModel : ViewModel() {
+
+}
